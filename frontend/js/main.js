6175c46d15996eb52009b2356f2ca7c44538d90f
Samartha-k <samartha.k.achar03@gmail.com>
2025-07-15 22:56:48 +0530

    Added frontend structure and base UI components

---
+++ frontend/js/main.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  fetch('../components/header.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('header').innerHTML = data);
+
+  fetch('../components/footer.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('footer').innerHTML = data);
+};
